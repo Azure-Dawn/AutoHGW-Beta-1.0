47ed0a80b7e0d571e2c66a4f1f1f97578ea1ba0d
AzureDawn25270 <121396236+Azure-Dawn@users.noreply.github.com>
2026-09-15 17:27:45 +0200

    Fix LabyGradle repository resolution for legacy LWJGL dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.api.initialization.resolve.RepositoriesMode
-
 rootProject.name = "labymod4-addon-template"
 
 pluginManagement {
@@ -15,35 +13,6 @@ pluginManagement {
 
         dependencies {
             classpath("net.labymod.gradle", "common", labyGradlePluginVersion)
-        }
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        mavenCentral()
-
-        exclusiveContent {
-            forRepository {
-                maven("https://dist.labymod.net/api/v1/maven/release/")
-            }
-            filter {
-                includeGroupByRegex("net\\.labymod(\\..*)?")
-            }
-        }
-
-        maven("https://maven.neoforged.net/releases/") {
-            content {
-                includeGroupByRegex("net\\.neoforged(\\..*)?")
-                includeGroupByRegex("com\\.mojang(\\..*)?")
-            }
-        }
-
-        maven("https://maven.fabricmc.net/") {
-            content {
-                includeGroupByRegex("net\\.fabricmc(\\..*)?")
-            }
         }
     }
 }
