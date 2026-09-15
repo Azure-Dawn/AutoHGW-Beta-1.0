20f0477f320ce9b79263de374d0fa32fac77f584
AzureDawn25270 <121396236+Azure-Dawn@users.noreply.github.com>
2026-09-15 19:44:18 +0200

    Move addon Java package to autohgws

---
+++ core/src/main/java/dev/azure/dawn/autohgws/AutoHGWAddon.java
@@ -0,0 +1,20 @@
+package dev.azure.dawn.autohgws;
+
+import net.labymod.api.addon.LabyAddon;
+import net.labymod.api.models.addon.annotation.AddonMain;
+
+@AddonMain
+public class AutoHGWAddon extends LabyAddon<AutoHGWConfiguration> {
+
+  @Override
+  protected void enable() {
+    this.registerSettingCategory();
+    this.registerListener(new AutoHGWTickListener(this));
+    this.logger().info("AutoHGW enabled");
+  }
+
+  @Override
+  protected Class<AutoHGWConfiguration> configurationClass() {
+    return AutoHGWConfiguration.class;
+  }
+}
