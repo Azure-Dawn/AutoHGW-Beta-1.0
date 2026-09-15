3af29e3b5b898d3ee3cfcdbc1ad2ae5a6fa7432d
AzureDawn25270 <121396236+Azure-Dawn@users.noreply.github.com>
2026-09-15 17:23:51 +0200

    Fix Gradle dependency repository resolution for LabyMod build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
 rootProject.name = "labymod4-addon-template"
 
 pluginManagement {
@@ -13,6 +15,35 @@ pluginManagement {
 
         dependencies {
             classpath("net.labymod.gradle", "common", labyGradlePluginVersion)
+        }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        mavenCentral()
+
+        exclusiveContent {
+            forRepository {
+                maven("https://dist.labymod.net/api/v1/maven/release/")
+            }
+            filter {
+                includeGroupByRegex("net\\.labymod(\\..*)?")
+            }
+        }
+
+        maven("https://maven.neoforged.net/releases/") {
+            content {
+                includeGroupByRegex("net\\.neoforged(\\..*)?")
+                includeGroupByRegex("com\\.mojang(\\..*)?")
+            }
+        }
+
+        maven("https://maven.fabricmc.net/") {
+            content {
+                includeGroupByRegex("net\\.fabricmc(\\..*)?")
+            }
         }
     }
 }
